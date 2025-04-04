0ec2fe69fe28cc276a0dd43626287f287d93042e
cuube1 <152514116+cuube1@users.noreply.github.com>
2025-04-04 19:04:02 +0100

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/ytai" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/ytai/" : "",
+};
 
-export default nextConfig;
+module.exports = nextConfig;
